0c99a3b26f7c94cbe4e79a815f296aedd3f37d04
Eshayat <133668606+ESHAYAT102@users.noreply.github.com>
2024-10-01 15:58:27 +0600

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of websites with React in that language. I also like Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/ESHAYAT102" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Frontend Web Development Certificate. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@FaceDevStuff" target="_blank">FaceDev</a>, <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>,
+  <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a> and <a href="https://www.youtube.com/@PCBuilderBangladesh" target="_blank">PC Builder Bangladesh</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to note it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my CV. <a href="https://github.com/ESHAYAT102/CV/blob/main/Eshayat%20CV.pdf" target="_blank">Check it out?</a>
+  Contact me at jslegend@protonmail.com if you have any collaboration ideas!`,
+  projects: `See my projects <a href="https://my-projects-eshayat.vercel.app" target="_blank"></a>`,
+  library: `There are a lot of programming books on my shelves. There are some in Bangla (I speak Bangla, it's my first language.).
+  I probably only read 3 of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Eshayat's portfolio, just close the tab.`,
+};
